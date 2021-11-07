f2521ea606f5d474c32c7b8670f970e8af7da4f8
g1348032 <76894697+g1348032@users.noreply.github.com>
2021-11-07 20:39:50 +0800

    Delete phpFile directory

--- phpFile/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username = "adminer";
-$password = "P@ssw0rd";
-$conn = new PDO('mysql:host=localhost;dbname=Movies', $username, $password);
-$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
